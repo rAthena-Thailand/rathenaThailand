115917d1602c556ef6334b46de9694cd44bac193
SamMee <sammeepay@hotmail.com>
2024-08-31 19:49:24 +0700

    ปรับ display_pinfo ให้ support PACKETVER 20211103
    ปรับ display_pinfo ให้ support PACKETVER 20211103

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -19944,10 +19944,6 @@ void clif_display_pinfo( map_session_data& sd ){
 
 		//1:Premium
 		if( pc_isvip( &sd ) ){
-			details_bexp[PINFO_PREMIUM] = battle_config.vip_base_exp_increase * battle_config.base_exp_rate / 100;
-			if (details_bexp[PINFO_PREMIUM] < 0)
-				details_bexp[PINFO_PREMIUM] = 0 - details_bexp[PINFO_PREMIUM];
-		} else
 			details_bexp[PINFO_PREMIUM] = 0;
 
 		//2:Server
@@ -19963,21 +19959,31 @@ void clif_display_pinfo( map_session_data& sd ){
 		}
 
 		//3:TPLUS
-		details_bexp[PINFO_CAFE] = 0;
+		//details_bexp[PINFO_CAFE] = 0;
+		if( pc_isvip( &sd ) ){
+			details_bexp[PINFO_CAFE] = battle_config.vip_base_exp_increase;
+			if (details_bexp[PINFO_CAFE] < 0)
+				details_bexp[PINFO_CAFE] = 0 - details_bexp[PINFO_CAFE];
+		} else
+			details_bexp[PINFO_CAFE] = 0;
 
 		/**
 		 * Drop rate
 		 */
 		//0:PCRoom
-		details_drop[PINFO_BASIC] = 0;
+		details_drop[PINFO_BASIC] = map_getmapflag( sd.bl.m, MF_BEXP );
+		if (details_drop[PINFO_BASIC] == 100 || !details_drop[PINFO_BASIC])
+			details_drop[PINFO_BASIC] = 0;
+		else {
+			if (details_drop[PINFO_BASIC] < 100) {
+				details_drop[PINFO_BASIC] = 100 - details_drop[PINFO_BASIC];
+				details_drop[PINFO_BASIC] = 0 - details_drop[PINFO_BASIC];
+			} else
+				details_drop[PINFO_BASIC] = details_drop[PINFO_BASIC] - 100;
+		}
 
 		//1:Premium
-		if( pc_isvip( &sd ) ){
-			details_drop[PINFO_PREMIUM] = (battle_config.vip_drop_increase * battle_config.item_rate_common) / 100;
-			if (details_drop[PINFO_PREMIUM] < 0)
-				details_drop[PINFO_PREMIUM] = 0 - details_drop[PINFO_PREMIUM];
-		} else
-			details_drop[PINFO_PREMIUM] = 0;
+		details_drop[PINFO_PREMIUM] = 0;
 
 		//2:Server
 		details_drop[PINFO_SERVER] = battle_config.item_rate_common;
@@ -19992,7 +19998,12 @@ void clif_display_pinfo( map_session_data& sd ){
 		}
 
 		//3:TPLUS
-		details_drop[PINFO_CAFE] = 0;
+		if( pc_isvip( &sd ) ){
+			details_drop[PINFO_CAFE] = battle_config.vip_base_exp_increase;
+			if (details_drop[PINFO_CAFE] < 0)
+				details_drop[PINFO_CAFE] = 0 - details_drop[PINFO_CAFE];
+		} else
+			details_drop[PINFO_CAFE] = 0;
 
 		/**
 		 * Penalty rate
@@ -20002,21 +20013,7 @@ void clif_display_pinfo( map_session_data& sd ){
 		details_penalty[PINFO_BASIC] = 0;
 
 		//1:Premium
-		if( pc_isvip( &sd ) ){
-			details_penalty[PINFO_PREMIUM] = battle_config.vip_exp_penalty_base;
-			if (details_penalty[PINFO_PREMIUM] == 100)
-				details_penalty[PINFO_PREMIUM] = 0;
-			else {
-				if (details_penalty[PINFO_PREMIUM] < 100) {
-					details_penalty[PINFO_PREMIUM] = 100 - details_penalty[PINFO_PREMIUM];
-					details_penalty[PINFO_PREMIUM] = 0 - details_penalty[PINFO_PREMIUM];
-				} else
-					details_penalty[PINFO_PREMIUM] = details_penalty[PINFO_PREMIUM] - 100;
-			}
-			if (battle_config.death_penalty_base > battle_config.vip_exp_penalty_base)
-				details_penalty[PINFO_PREMIUM] = battle_config.vip_exp_penalty_base - battle_config.death_penalty_base;
-		} else
-			details_penalty[PINFO_PREMIUM] = 0;
+		details_penalty[PINFO_PREMIUM] = 0;
 
 		//2:Server
 		details_penalty[PINFO_SERVER] = battle_config.death_penalty_base;
@@ -20031,7 +20028,21 @@ void clif_display_pinfo( map_session_data& sd ){
 		}
 
 		//3:TPLUS
-		details_penalty[PINFO_CAFE] = 0;
+		if( pc_isvip( &sd ) ){
+			details_penalty[PINFO_CAFE] = battle_config.vip_exp_penalty_base;
+			if (details_penalty[PINFO_CAFE] == 100)
+				details_penalty[PINFO_CAFE] = 0;
+			else {
+				if (details_penalty[PINFO_CAFE] < 100) {
+					details_penalty[PINFO_CAFE] = 100 - details_penalty[PINFO_CAFE];
+					details_penalty[PINFO_CAFE] = 0 - details_penalty[PINFO_CAFE];
+				} else
+					details_penalty[PINFO_CAFE] = details_penalty[PINFO_CAFE] - 100;
+			}
+			if (battle_config.death_penalty_base > battle_config.vip_exp_penalty_base)
+				details_penalty[PINFO_CAFE] = battle_config.vip_exp_penalty_base - battle_config.death_penalty_base;
+		} else
+			details_penalty[PINFO_CAFE] = 0;
 
 	PACKET_ZC_PERSONAL_INFOMATION* p = reinterpret_cast<PACKET_ZC_PERSONAL_INFOMATION*>( packet_buffer );
 
@@ -21583,7 +21594,7 @@ void clif_attendence_response( map_session_data *sd, int32 data ){
 
 	WFIFOHEAD(fd,packet_len(0xAF0));
 	WFIFOW(fd,0) = 0xAF0;
-	WFIFOL(fd,2) = 0;
+	WFIFOL(fd,2) = battle_config.feature_attendance_close;
 	WFIFOL(fd,6) = data;
 	WFIFOSET(fd,packet_len(0xAF0));
 }
