f003c36f3a394b339158af7370381653e0db8046
SamMee <sammeepay@hotmail.com>
2025-12-31 21:33:01 +0700

    Implemented new items up to Dec 3, 2025 (#9700)
    Implemented new items up to Dec 3, 2025 (#9700)
    Berserk vs. Dispell/Clearance (#9704)
    Fix HC_NOTIFY_ZONESVR packet header (#9703)
    Added Grade fild in item_packages.yml (#9687)
    Corrected typo and item script format (#9697)
    Corrected item combo Frontier Rune Crown (Abyss Chaser) + Frontier Abyss Dagger (#9696)
    Updated training zones (#9667)
    Implemented new items up to Nov 5, 2025 (#9663)
    Bodystyle logic in clif_spawn_unit
    Restored atcommand reload (#9669)
    Implementation of bNonCritAtkRate Bonus (#9532)
    Fixed bodystyle on older clients (#9657)
    Fixed forum URL (#9694)
    Corrected Arch Mage and Elemental Master skills ratio (#9672)
    Rebalance Shinkiro & Shiranui 2024.02.05 (#9681)
    Corrected stat bonuses for kagerou/oboro/shinkiro/shiranui (#9683)
    Rebalance shinkiro 20221221 (#9679)
    Corrected changesex new bodystyle behavior (#9673)
    Correction of Rock Ridge NPCs script (#9686)
    Corrected item group DB node name (#9680)
    Corrected Handling of 2nd Upper Expanded Jobs / Fixed Skill Tree Issues(#9661)

--- src/map/itemdb.cpp
+++ src/map/itemdb.cpp
@@ -2872,6 +2872,29 @@ uint64 ItemPackageDatabase::parseBodyNode( const ryml::NodeRef& node ){
 					}
 				}
 
+				if (this->nodeExists(itemNode, "Grade")) {
+					std::string enchantgrade;
+
+					if (!this->asString(itemNode, "Grade", enchantgrade)) {
+						return 0;
+					}
+
+					std::string grade_constant = "ENCHANTGRADE_" + enchantgrade;
+					int64 constant;
+
+					if (!script_get_constant(grade_constant.c_str(), &constant) || constant < ENCHANTGRADE_NONE ||	constant > MAX_ENCHANTGRADE) {
+						this->invalidWarning(itemNode["Grade"], "Invalid enchantgrade %s, defaulting to None.\n", enchantgrade.c_str());
+						constant = ENCHANTGRADE_NONE;
+					}
+
+					package_item->grade = static_cast<decltype(package_item->grade)>(constant);
+				}
+				else {
+					if (!package_item_exists) {
+						package_item->grade = static_cast<decltype(package_item->grade)>(ENCHANTGRADE_NONE);
+					}
+				}
+
 				if( !package_item_exists ){
 					group->items[package_item->item_id] = package_item;
 				}
@@ -3660,10 +3683,10 @@ uint64 ItemGroupDatabase::parseBodyNode(const ryml::NodeRef& node) {
 					}
 				}
 
-				if (this->nodeExists(listit, "MinimumEnchantgrade")) {
+				if (this->nodeExists(listit, "GradeMinimum")) {
 					std::string enchantgrade;
 
-					if (!this->asString(listit, "MinimumEnchantgrade", enchantgrade)) {
+					if (!this->asString(listit, "GradeMinimum", enchantgrade)) {
 						return 0;
 					}
 
@@ -3671,7 +3694,7 @@ uint64 ItemGroupDatabase::parseBodyNode(const ryml::NodeRef& node) {
 					int64 constant;
 
 					if (!script_get_constant(grade_constant.c_str(), &constant) || constant < ENCHANTGRADE_NONE || constant > MAX_ENCHANTGRADE) {
-						this->invalidWarning(listit["MinimumEnchantgrade"], "Invalid enchantgrade %s, defaulting to None.\n", enchantgrade.c_str());
+						this->invalidWarning(listit["GradeMinimum"], "Invalid enchantgrade %s, defaulting to None.\n", enchantgrade.c_str());
 						constant = ENCHANTGRADE_NONE;
 					}
 
@@ -3683,10 +3706,10 @@ uint64 ItemGroupDatabase::parseBodyNode(const ryml::NodeRef& node) {
 					}
 				}
 
-				if (this->nodeExists(listit, "MaximumEnchantgrade")) {
+				if (this->nodeExists(listit, "GradeMaximum")) {
 					std::string enchantgrade;
 
-					if (!this->asString(listit, "MaximumEnchantgrade", enchantgrade)) {
+					if (!this->asString(listit, "GradeMaximum", enchantgrade)) {
 						return 0;
 					}
 
@@ -3694,7 +3717,7 @@ uint64 ItemGroupDatabase::parseBodyNode(const ryml::NodeRef& node) {
 					int64 constant;
 
 					if (!script_get_constant(grade_constant.c_str(), &constant) || constant < ENCHANTGRADE_NONE || constant > MAX_ENCHANTGRADE) {
-						this->invalidWarning(listit["MaximumEnchantgrade"], "Invalid enchantgrade %s, defaulting to None.\n", enchantgrade.c_str());
+						this->invalidWarning(listit["GradeMaximum"], "Invalid enchantgrade %s, defaulting to None.\n", enchantgrade.c_str());
 						constant = ENCHANTGRADE_NONE;
 					}
 
